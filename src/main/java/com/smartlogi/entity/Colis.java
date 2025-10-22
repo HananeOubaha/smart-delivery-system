e277d30f166f76599af4483ba2aa3c7002a6f12f
Hanane Oubaha <Hananeouba03@gmail.com>
2025-10-22 12:10:08 +0100

    feat : j'aicreer les entités livreur et colis ainsi l'enum https://hananeoubaha.atlassian.net/browse/SDMS-2

---
+++ src/main/java/com/smartlogi/entity/Colis.java
@@ -0,0 +1,89 @@
+package com.smartlogi.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "colis")
+public class Colis implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String destinataire;
+    private String adresse;
+    private Double poids;
+
+    @Enumerated(EnumType.STRING) // Stocke l'enum en tant que String ("EN_TRANSIT") et non en nombre (1)
+    private StatutColis statut;
+
+    // --- Relation Many-to-One ---
+    // C'est l'entité "propriétaire".
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "livreur_id", nullable = false) // Crée la colonne "livreur_id" dans la table "colis"
+    private Livreur livreur;
+
+    // --- Constructeurs ---
+    public Colis() {
+    }
+
+    public Colis(String destinataire, String adresse, Double poids, StatutColis statut, Livreur livreur) {
+        this.destinataire = destinataire;
+        this.adresse = adresse;
+        this.poids = poids;
+        this.statut = statut;
+        this.livreur = livreur;
+    }
+
+    // --- Getters et Setters ---
+    // (Générez-les avec votre IDE)
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDestinataire() {
+        return destinataire;
+    }
+
+    public void setDestinataire(String destinataire) {
+        this.destinataire = destinataire;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public Double getPoids() {
+        return poids;
+    }
+
+    public void setPoids(Double poids) {
+        this.poids = poids;
+    }
+
+    public StatutColis getStatut() {
+        return statut;
+    }
+
+    public void setStatut(StatutColis statut) {
+        this.statut = statut;
+    }
+
+    public Livreur getLivreur() {
+        return livreur;
+    }
+
+    public void setLivreur(Livreur livreur) {
+        this.livreur = livreur;
+    }
+}
---
+++ src/main/java/com/smartlogi/entity/Livreur.java
@@ -0,0 +1,95 @@
+package com.smartlogi.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "livreurs") // Nom de la table en BDD
+public class Livreur implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Laisse PostgreSQL gérer l'auto-incrémentation
+    private Long id;
+
+    private String nom;
+    private String prenom;
+    private String vehicule;
+    private String telephone;
+
+    // --- Relation One-to-Many ---
+    // mappedBy = "livreur" : Dit à JPA que c'est le champ "livreur"
+    // dans la classe "Colis" qui gère la clé étrangère.
+    // Le Livreur n'a pas de colonne "colis_id".
+    @OneToMany(
+            mappedBy = "livreur",
+            cascade = CascadeType.ALL, // Si on supprime un livreur, ses colis sont supprimés
+            orphanRemoval = true,      // Si on enlève un colis de la liste, il est supprimé
+            fetch = FetchType.LAZY     // Ne charge les colis que si on le demande (performance)
+    )
+    private List<Colis> colis = new ArrayList<>();
+
+    // --- Constructeurs ---
+    // JPA a besoin d'un constructeur vide
+    public Livreur() {
+    }
+
+    public Livreur(String nom, String prenom, String vehicule, String telephone) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.vehicule = vehicule;
+        this.telephone = telephone;
+    }
+
+    // --- Getters et Setters ---
+    // Vous pouvez les générer avec votre IDE (Alt+Insert > Getters and Setters)
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getVehicule() {
+        return vehicule;
+    }
+
+    public void setVehicule(String vehicule) {
+        this.vehicule = vehicule;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public List<Colis> getColis() {
+        return colis;
+    }
+
+    public void setColis(List<Colis> colis) {
+        this.colis = colis;
+    }
+}
